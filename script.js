193bde5175c32a20cf17172991b88ee540ba5caa
Abhiram <yadatoreabhiram@gmail.com>
2024-01-20 14:06:11 +0530

    portfolio website

---
+++ script.js
@@ -0,0 +1,13 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth", // Use 'smooth' to enable smooth scrolling
+  });
+}
